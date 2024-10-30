634de2b62b0a7ebc1b13eb3b1832cc4d7d68c829
MatinMulla <114789200+MatinMulla@users.noreply.github.com>
2024-10-30 17:59:08 +0000

    Datatypes

---
+++ Basics_01/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //It treats all Js code as a Newer Version
+
+// alert( 3 + 3 ) // Basically We are Using NodeJS so this will not work, It works only on Browser 
+
+console.log("Matin")
+
+let name = "Abdulmatin"
+let age = 21
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//Boolean = true/false
+//null => Standalone value/ empty
+//undefined =>
+//symbol => it is used to find Uniqueness 
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //Object
+
