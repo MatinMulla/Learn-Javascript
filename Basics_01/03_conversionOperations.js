a1a37d27cc7777b607f256c05e93c4b0fec016e8
MatinMulla <114789200+MatinMulla@users.noreply.github.com>
2024-10-31 11:02:31 +0000

    ConversionOperations

---
+++ Basics_01/03_conversionOperations.js
@@ -0,0 +1,30 @@
+let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let ValueInNumber = Number(score)
+//console.log(typeof ValueInNumber);
+//console.log(ValueInNumber);
+
+
+// "33" =>  will convert into 33
+//"33abc" => NaN
+//true values converted as 1 and false values as 0
+
+let isLoggedIn = "absdf"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true and 0=> false
+// if I put Emplty ( "" ) => value is returned will be false
+// if I give any String such as ("Matin") => it will be true.
+
+let someNumber = 12
+
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
+
